20ce6a6ee2f92e91417cef9b293577ae57b5a371
hesham <ehesham26@yahoo.com>
2023-02-07 03:38:28 +0200

    fix some issiues

--- src/pages/PokemonDetails.jsx
+++ src/pages/PokemonDetails.jsx
@@ -31,7 +31,7 @@ const PokemonDetails = () => {
           paddingTop: "7px",
           paddingBottom: "7px",
         }}
-        onClick={() => navigate("pokemon_task/")}
+        onClick={() => navigate(-1)}
       >
         <ArrowBackIosIcon style={{ fontSize: "12px", marginRight: "10px" }} />{" "}
         Back
